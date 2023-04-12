9be5370d9a3e0328c0f0b80ee192ff47514ed934
Mitja Polner <mitja.polner.2001@gmail.com>
2023-04-12 18:56:41 +0200

    Added basic Express server

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('FastTyper backend test');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
