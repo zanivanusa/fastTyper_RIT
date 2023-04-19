b9d18e7ed731aa3bf14577f29e2fd2021c5fb293
Mitja Polner <mitja.polner.2001@gmail.com>
2023-04-19 21:17:37 +0200

    Added API endpoint for returning Lorem ipsum text

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('FastTyper backend test');
 });
 
+app.get('/text', (req, res) => {
+  const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor, nisl eget ultricies tincidunt, nisl nisl aliquam nisl, eget aliqu';
+  return res.json({ text });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
